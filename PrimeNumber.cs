9d597ebbace585cceb8f20a1605a718fb0732614
navabhi22 <navabhi22@gmail.com>
2022-02-23 20:52:28 +0530

    Added class to Check for Prime Number

---
+++ PrimeNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    public class PrimeNumber
+    {
+        /// <summary>
+        /// Checks for the PrimeNumber.
+        /// </summary>
+        /// <param name="N">The n.</param>
+        public static void PrimeNum(int N)
+        {
+            int numOfFact = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                int rimainder = N % i;
+
+                if (rimainder == 0)
+                {
+                    numOfFact++;
+                }
+
+            }
+
+            if (numOfFact == 2)
+            {
+                Console.WriteLine("prime");
+            }
+            else
+            {
+                Console.WriteLine("not prime");
+            }
+        }
+    }
+}
