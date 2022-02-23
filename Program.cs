ed2b0f418d32113368b6e4beeb5716bb2e99f15d
navabhi22 <navabhi22@gmail.com>
2022-02-23 20:30:18 +0530

    Day6_LogicaProgram

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day6_LogicalProgram");
+
+        }
+    }
+}
