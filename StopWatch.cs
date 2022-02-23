4a53704918088d2342238041c50b6e0a78d57643
navabhi22 <navabhi22@gmail.com>
2022-02-23 21:04:14 +0530

    Added class to Simulate StopWatch

---
+++ StopWatch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+public class ProgramStopWatch
+{
+	public static void StopWatch()
+	{
+		// Create new stopwatch
+		Stopwatch stopwatch = new Stopwatch();
+
+		// Begin timing
+		stopwatch.Start();
+
+		// Do something
+		for (int i = 0; i < 100; i++)
+		{
+			Thread.Sleep(1);
+		}
+
+		// Stop timing
+		stopwatch.Stop();
+
+		// Write result
+		Console.WriteLine("Time elapsed: {0}",
+			stopwatch.Elapsed);
+	}
+}
